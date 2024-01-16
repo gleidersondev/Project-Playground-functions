39658f14191a3823b821592ac05f82ab367c3ad9
gleidersondev <gleidersondev@gmail.com>
2024-01-16 20:48:05 -0300

    Feito desafio 03

--- challenge-03.js
+++ challenge-03.js
@@ -2,12 +2,26 @@
 // Implemente a função splitSentence que divide uma frase de acordo com a quantidade de palavras
 
 // A função `splitSentence` recebe uma string como parâmetro e deve retornar um array com as palavras separadas por vírgula.
-// Exemplo: se a função receber a string 'go Guys', o retorno deverá ser ['go', 'Guys'].
+// Exemplo: se a função receber a string 'Vamos Flamengo', o retorno deverá ser ['Vamos', 'Flamengo'].
 
 // O que será testado:
 
-// Retorne o valor ['go', 'Guys'] se a função receber a string 'go Guys';
+// Retorne o valor ['Vamos', 'Flamengo'] se a função receber a string 'Vamos Flamengo';
 
 // Retorne o valor ['vamo', 'que', 'vamo'] se a função receber a string 'vamo que vamo';
 
 // Retorne o valor ['foguete'] se a função receber a string 'foguete'.
+
+const flamengo = 'Vamos Flamengo';
+
+const vamo = 'vamo que vamo';
+
+const praCima = 'foguete';
+
+const splitSentence = (string) => string.split(' ');
+
+console.log(splitSentence(flamengo));
+
+console.log(splitSentence(vamo));
+
+console.log(splitSentence(praCima));

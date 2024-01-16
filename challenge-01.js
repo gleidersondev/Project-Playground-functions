dff9c2ff4472bd5bff5cd2335331b9f8a668bb60
gleidersondev <gleidersondev@gmail.com>
2024-01-16 20:11:04 -0300

    Feito desafio 01

--- challenge-01.js
+++ challenge-01.js
@@ -1,4 +1,4 @@
-// Implemente a função compareTrue utilizando somente o operador &&
+// Implemente a função  utilizando somente o operador &&
 
 // A função compareTrue ao receber dois parâmetros booleanos deve:
 
@@ -6,7 +6,14 @@
 // Retornar false se um ou ambos os parâmetros forem falsos.
 // Exemplo:
 
-// const girafa = true;
-// const elefante = true;
-// const macaco = false;
+const girafa = true;
+const elefante = true;
+const macaco = false;
+
 // Se a função for chamada com os valores girafa e elefante como parâmetro, retorna true, mas caso seja chamada com os parâmetro macaco e elefante retorna false.
+
+const compareTrue = (paran1, paran2) => paran1 && paran2;
+
+console.log(compareTrue(girafa, elefante));
+
+console.log(compareTrue(macaco, elefante));
